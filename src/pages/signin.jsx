d70a2266613e1c5e46703c6f96a4f1255d489032
vanh01 <ngvanhfw@gmail.com>
2022-03-13 13:30:13 +0700

    update code

---
+++ src/pages/signin.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/navbar/navbar";
+
+const signin = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+};
+
+export default signin;

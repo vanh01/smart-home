2aa8764144480d674e63dfa5fa7f92cdb5a70078
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2022-03-16 16:11:04 +0700

    set up log

--- src/components/ActivityLog/ActivityLog.jsx
+++ src/components/ActivityLog/ActivityLog.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ActivityLog = () => {
-    return <div>ActivityLog</div>;
+
+    return <div className="activity-log">
+        <div className="activity__button-list">
+            
+        </div>
+            adsasd
+    </div>
+
+
 };
 
 export default ActivityLog;

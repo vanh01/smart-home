64a1e40144730cb85a78b984842b1500133679c4
PhongxTran <92942665+PhongxTran@users.noreply.github.com>
2022-03-21 20:48:54 +0700

    commit fe

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import DashboardPage from "./pages/DashboardPage"
 import ActivitylogPage from "./pages/ActivityLogPage"
 import ManageAccountPage from "./pages/ManageAccountPage";
 import ForgotPassword from "./components/Signin/ForgotPassword";
+import ManagerPage from "./pages/ManagerPage";
 import { Route, Routes } from "react-router-dom";
 import './css/activity.css'
 import './css/signin.css'
@@ -12,6 +13,7 @@ import './css/dashboard.css'
 import './css/custom.css'
 import './css/forgot.css'
 import './css/apartment.css'
+import './css/manager.css'
 
 function App() {
     return (
@@ -21,6 +23,7 @@ function App() {
             <Route path="/dashboard" exact element={<DashboardPage />} />
             <Route path="/activitylog" exact element={<ActivitylogPage />} />
             <Route path="/manageaccount" exact element={<ManageAccountPage />} />
+            <Route path="/manager" exact element={<ManagerPage />} />
         </Routes>
     );
 }
